51acd9192666bc7c396df1e63692c537873e2a77
Your Name <you@example.com>
2021-01-29 10:22:57 +0100

    objets2

---
+++ objets.py
@@ -0,0 +1,8 @@
+objets = { '*' : ('0r' , 0)  ,
+          'j' : ('potion magique' , 5) ,
+          '!' : ('épées' , 3) ,
+          '(' : ('arc et flèches' , 3) ,
+          '&' : ('armure' , 3) ,
+          '£' : ('parchemins' , 2)
+          '¤' : ('anneau de pouvoir' , 5) ,
+          'µ' : ('nourriture' , 2) }
